5211acdcae2fb229da5a1acffdda8fd3f9aa5c2f
167613A <A167613@163.com>
2026-07-15 11:16:31 +0800

    a0.2.2 update

--- blocks.cpp
+++ blocks.cpp
@@ -36,6 +36,58 @@ Matrix::~Matrix()
 	matrixDeallocate();
 }
 
+int& Matrix::at(int r, int c) {return data[r][c];}
+int Matrix::getRow() {return row;}
+int Matrix::getCol() {return col;}
+void Matrix::setRow(int r) {row = r;}
+void Matrix::setCol(int c) {col = c;}
+
+Matrix Matrix::matrixRotate() const
+{
+	Matrix res(col, row);
+	for(int i = 0; i < row; i++)
+	{
+		for(int j = 0; j < col; j++)
+		{
+			res.data[j][row - 1 - i] = data[i][j];
+		}
+	}
+	return res;
+}
+
+void Matrix::matrixPrint() const
+{
+	for(int i = 0; i < row; i++)
+	{
+		for(int j = 0; j < col; j++)
+		{
+			cout << data[i][j] << ' ';
+		}
+		cout << endl;
+	}
+}
+
+Matrix& Matrix::operator=(const Matrix& mat)
+{
+	if(this == &mat)
+	{
+		return *this;
+	}
+	
+	matrixDeallocate();
+	row = mat.row;
+	col = mat.col;
+	matrixAllocate(mat.row, mat.col);
+	for(int i = 0; i < row; i++)
+	{
+		for(int j = 0; j < col; j++)
+		{
+			data[i][j] = mat.data[i][j];
+		}
+	}
+	return *this;
+}
+
 int Block::getBlockPosX() {return posX;}
 int Block::getBlockPosY() {return posY;}
 
@@ -53,20 +105,36 @@ void Parts::partRotate()
 	{
 	case 0:
 		shapeRotated = shapeOrigin.matrixRotate();
+		shapeCurrent = shapeRotated;
 		break;
 	case 1:
 		shapeOrigin = shapeRotated.matrixRotate();
+		shapeCurrent = shapeOrigin;
 		break;
 	default:
 		break;
 	}
 }
 
-void Parts::partDrop()
+void Parts::partSoftDrop()
 {
-	
+	;
 }
 
+void Parts::partHardDrop()
+{
+	;
+}
+
+void Parts::partMoveLeft()
+{
+	if(partLinePosOrigin)
+	{
+		;
+	}
+}
+
+void Parts::partMoveRight() {movesRight++;}
 
 Game::Game(int s, int t, double d) : score(s), timer(t), difficulty(d), currentPart(Parts(WHITE, 0))
 {
@@ -103,38 +171,53 @@ void Game::gameDrawHomeUI()
 
 void Game::newPart()
 {
-	switch(rand() % 7) 
+	switch(rand() % 8) 
 	{
-	case 0:
-		currentPart = PartZ(CYAN);
-		break;
 	case 1:
-		currentPart = PartS(MAGENTA);
+		currentPart = PartZ(CYAN);
+		movingPart = true;
 		break;
 	case 2:
-		currentPart = PartL(RED);
+		currentPart = PartS(MAGENTA);
+		movingPart = true;
 		break;
 	case 3:
-		currentPart = PartJ(ORANGE);
+		currentPart = PartL(RED);
+		movingPart = true;
 		break;
 	case 4:
-		currentPart = PartO(YELLOW);
+		currentPart = PartJ(ORANGE);
+		movingPart = true;
 		break;
 	case 5:
-		currentPart = PartT(GREEN);
+		currentPart = PartO(YELLOW);
+		movingPart = true;
 		break;
 	case 6:
+		currentPart = PartT(GREEN);
+		movingPart = true;
+		break;
+	case 7:
 		currentPart = PartI(BLUE);
+		movingPart = true;
 		break;
 	default:
+		movingPart = false;
 		break;
 	}
 }
+
+
 
 PartZ::PartZ(COLORREF clr) : Parts(clr, 0)
 {
 	shapeOrigin = Matrix(2, 3);
 	shapeRotated = Matrix(3, 2);
+	shapeOrigin.at(0, 0) = 1;
+	shapeOrigin.at(0, 1) = 1;
+	shapeOrigin.at(1, 1) = 1;
+	shapeOrigin.at(1, 2) = 1;
+	shapeCurrent = shapeOrigin;
 }
 PartZ::~PartZ()
 {
@@ -146,6 +229,11 @@ PartS::PartS(COLORREF clr) : Parts(clr, 0)
 {
 	shapeOrigin = Matrix(2, 3);
 	shapeRotated = Matrix(3, 2);
+	shapeOrigin.at(0, 1) = 2;
+	shapeOrigin.at(0, 2) = 2;
+	shapeOrigin.at(1, 0) = 2;
+	shapeOrigin.at(1, 1) = 2;
+	shapeCurrent = shapeOrigin;
 }
 PartS::~PartS()
 {
@@ -157,6 +245,11 @@ PartL::PartL(COLORREF clr) : Parts(clr, 0)
 {
 	shapeOrigin = Matrix(2, 3);
 	shapeRotated = Matrix(3, 2);
+	shapeOrigin.at(0, 2) = 3;
+	shapeOrigin.at(1, 0) = 3;
+	shapeOrigin.at(1, 1) = 3;
+	shapeOrigin.at(1, 2) = 3;
+	shapeCurrent = shapeOrigin;
 }
 PartL::~PartL()
 {
@@ -168,12 +261,27 @@ PartJ::PartJ(COLORREF clr) : Parts(clr, 0)
 {
 	shapeOrigin = Matrix(2, 3);
 	shapeRotated = Matrix(3, 2);
+	shapeOrigin.at(0, 0) = 4;
+	shapeOrigin.at(1, 0) = 4;
+	shapeOrigin.at(1, 1) = 4;
+	shapeOrigin.at(1, 2) = 4;
+	shapeCurrent = shapeOrigin;
+}
+PartJ::~PartJ()
+{
+	shapeOrigin.~Matrix();
+	shapeRotated.~Matrix();
 }
 
 PartO::PartO(COLORREF clr) : Parts(clr, 0)
 {
 	shapeOrigin = Matrix(2, 2);
 	shapeRotated = Matrix(2, 2);
+	shapeOrigin.at(0, 0) = 5;
+	shapeOrigin.at(0, 1) = 5;
+	shapeOrigin.at(1, 0) = 5;
+	shapeOrigin.at(1, 1) = 5;
+	shapeCurrent = shapeOrigin;
 }
 PartO::~PartO()
 {
@@ -185,6 +293,11 @@ PartT::PartT(COLORREF clr) : Parts(clr, 0)
 {
 	shapeOrigin = Matrix(2, 3);
 	shapeRotated = Matrix(3, 2);
+	shapeOrigin.at(0, 1) = 6;
+	shapeOrigin.at(1, 0) = 6;
+	shapeOrigin.at(1, 1) = 6;
+	shapeOrigin.at(1, 2) = 6;
+	shapeCurrent = shapeOrigin;
 }
 PartT::~PartT()
 {
@@ -196,6 +309,11 @@ PartI::PartI(COLORREF clr) : Parts(clr, 0)
 {
 	shapeOrigin = Matrix(4, 1);
 	shapeRotated = Matrix(1, 4);
+	shapeOrigin.at(0, 0) = 7;
+	shapeOrigin.at(1, 0) = 7;
+	shapeOrigin.at(2, 0) = 7;
+	shapeOrigin.at(3, 0) = 7;
+	shapeCurrent = shapeOrigin;
 }
 PartI::~PartI()
 {
